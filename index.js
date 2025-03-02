7cb77b023618b73598d3368f94e3ac4002a1ed05
Peter kanyori <peterynaenah23@gmail.com>
2025-03-02 19:08:07 +0300

    modification

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+if (main) {
+  main.remove();
+}
+
+const yourName = "Peter Wainaina";  // You can change this to whatever name you want
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';  // Make sure the id is set to 'victory'
+newHeader.textContent = `${yourName} is the champion`;  // Ensure the text matches the format
+document.body.appendChild(newHeader);
